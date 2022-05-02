35afb03ef90b9034302da51bce651f3fad424070
xuantrinh151 <79952017+xuantrinh151@users.noreply.github.com>
2022-05-02 15:59:59 +0700

    add recursion to see GREEN 4EVER --regression testing

--- src/com/trinhnx151/mathutil/core/MathUtil.java
+++ src/com/trinhnx151/mathutil/core/MathUtil.java
@@ -34,13 +34,11 @@ public class MathUtil {
     //5! = 5 * 4!
     //......
     //1! = 1 dừng
-    public static long getFactorial(int n) throws IllegalAccessException{
-        if(n < 0 || n > 20){
-            throw new IllegalAccessException("n must be between 0..20");
-        }
-        if(n == 0 || n == 1){
+    public static long getFactorial(int n) {
+        if(n < 0 || n > 20)
+          throw new IllegalArgumentException("n must be between 0..20");
+        if(n == 0 || n == 1)
             return 1;
-        }
         return n * getFactorial(n-1);
     }
 }
