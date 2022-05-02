17dab158ca3ec3b088cc3d7590f16ce82c9454dc
xuantrinh151 <79952017+xuantrinh151@users.noreply.github.com>
2022-05-02 15:57:16 +0700

    add recursion to see GREEN 4EVER --regression testing

--- src/com/trinhnx151/mathutil/core/MathUtil.java
+++ src/com/trinhnx151/mathutil/core/MathUtil.java
@@ -16,17 +16,31 @@ public class MathUtil {
     //tính giai thừa trong khoảng từ 0 đến 20 
     //Ta xài luôn kĩ thuật lập trinh tdd(test first development)
     //Nghĩa là ta nghĩ về bộ dữ liệu test trước khi viết code
-    public static long getFactorial(int n) {
-        long product = 1;
-        if(n < 0 || n > 20)
-            throw new IllegalArgumentException("n must be between 0..20");
-        if(n == 0 || n ==1)
-            return 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
+//    public static long getFactorial(int n) {
+//        long product = 1;
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        if(n == 0 || n ==1)
+//            return 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//
+//        return product;
+//    }
+    
+    //Đệ quy , recursion , gọi lại chính mình với quy mô khác
+    //Tính 5!
+    //5! = 5 * 4!
+    //......
+    //1! = 1 dừng
+    public static long getFactorial(int n) throws IllegalAccessException{
+        if(n < 0 || n > 20){
+            throw new IllegalAccessException("n must be between 0..20");
         }
-
-        return product;
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return n * getFactorial(n-1);
     }
-
 }
